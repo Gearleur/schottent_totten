83881c51269055e0178e27bb3c5863fedd789283
Teixeira Alexandre <alexandretei13@gmail.com>
2023-05-10 13:36:37 +0200

    class

---
+++ Board/board.h
@@ -0,0 +1,14 @@
+//
+// Created by alexa on 10/05/2023.
+//
+
+#ifndef SCHOTTENT_TOTTEN_BOARD_H
+#define SCHOTTENT_TOTTEN_BOARD_H
+
+
+class board {
+
+};
+
+
+#endif //SCHOTTENT_TOTTEN_BOARD_H
---
+++ Player/player.h
@@ -0,0 +1,14 @@
+//
+// Created by alexa on 10/05/2023.
+//
+
+#ifndef SCHOTTENT_TOTTEN_PLAYER_H
+#define SCHOTTENT_TOTTEN_PLAYER_H
+
+
+class player {
+
+};
+
+
+#endif //SCHOTTENT_TOTTEN_PLAYER_H
