7c75bdb85b6136c888f50b1303987cb128fb9e0f
Teixeira Alexandre <alexandretei13@gmail.com>
2023-05-15 13:19:47 +0200

    up

--- Card/Card.cpp
+++ Card/Card.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "Card.h"
+#include "../Utils/enums.h"
+#include "../Player/Player.h"
+
+Card::Card(Numbered number, Color color, Type type, char* name, char* description, Player* owner, Effect effect):
+        number(number), color(color), type(type), name(name), description(description), owner(owner), effect(effect) {};
+
--- Card/Card.h
+++ Card/Card.h
@@ -5,8 +5,50 @@
 #ifndef SCHOTTENT_TOTTEN_CARD_H
 #define SCHOTTENT_TOTTEN_CARD_H
 
+#include "../Utils/enums.h"
+#include "../Player/Player.h"
 
+//la class card est la classe abstraite qui contient les fonctions principales pour generer une carte
+//cette classe abstraite engendre deux classe filles qui sont les deux types de cartes
+//Il y a les cartes numérotées et les cartes spéciales
+//les cartes numérotées sont les cartes de 1 à 9 et 6 couleurs différentes
+//les cartes spéciales sont des cartes avec des pouvoirs spéciaux
+//on utilise les
 class Card {
+    protected:
+        Numbered number;
+        Color color;
+        Type type;
+        char* name;
+        char* description;
+        Player* owner = nullptr;
+
+    public:
+        Card(Numbered number, Color color, Type type, char* name, char* description, Player* owner, Effect effect):
+            number(number), color(color), type(type), name(name), description(description), owner(owner){};
+
+        Card() {};
+        virtual ~Card()=default; //destructor en virtual car Card Abstract
+
+        //getters
+        Numbered getNumber() const { return number; };
+        Color getColor() const { return color; };
+        Type getType() const { return type; };
+        char* getName() const { return name; };
+        char* getDescription() const { return description; };
+        Player* getOwner() const { return owner; };
+
+        //setters
+        void setNumber(Numbered number) { this->number = number; };
+        void setColor(Color color) { this->color = color; };
+        void setType(Type type) { this->type = type; };
+        void setOwner(Player* owner) { this->owner = owner; };
+
+        //fonctions
+
+
+
+
 
 };
 
